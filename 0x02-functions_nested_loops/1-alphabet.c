0e8cf3dbfb3766a1762082e59750e3a17dcee56c
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-03-15 20:40:05 +0100

    Print alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: Print alphabet in lower case followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+	
+
+	for (i ='a'; i = 'z'; i++)
+	{
+		_putchar(i)
+	}
+}
