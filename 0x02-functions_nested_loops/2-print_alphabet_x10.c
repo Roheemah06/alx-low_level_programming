dbddd61e93f5ba053c68748993f142f6748530b3
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-03-16 08:40:32 +0100

    10x alphabet in lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Print_alphabet_x10 - writes the alphabet 10x to sdtout
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
