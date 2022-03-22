bd42f03d60a09273a29bd46beedee11602bc78df
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-03-22 06:04:19 +0100

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,12 @@ void rev_string(char *s)
 	int j = 0;
 	char rev[1000];
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 		i++;
 
 	for (; i > 0; i--)
 	{
-		rev[j] = s[i-1];
+		rev[j] = s[i - 1];
 		j++;
 	}
 
