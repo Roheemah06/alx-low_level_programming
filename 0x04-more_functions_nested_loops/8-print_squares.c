68b6ecb342c2d7a8f9cf02e026ebbeb68dab63b5
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-03-23 02:09:46 +0100

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_squares.c
@@ -0,0 +1,21 @@
+#includee "main.h"
+
+/**
+ * print_square - print a square according to the size
+ * @size: dimensions pf square
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
