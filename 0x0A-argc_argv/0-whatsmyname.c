3d60037a7c28497044e632f4bd0ff9a13978c7b2
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-04-02 03:55:09 +0100

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: array of arguments entered
+ * Return: Always 0 (Success)
+ **/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+}
