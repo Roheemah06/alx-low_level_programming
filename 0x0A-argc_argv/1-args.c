aa8329e94fdd1bfd57117823545c09a7d44e4097
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-04-02 03:57:35 +0100

    prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: array of arguments entered
+ * Return: Always 0 (Success)
+ **/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+
+	return (0);
+}
