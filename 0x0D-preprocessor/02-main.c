28a4c6675917a87c970f5af8a3e44c9319fb0ebb
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-04-12 23:38:04 +0100

    print name of file

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#ifndef __FILE__
+#define __FILE__
+#endif /* __FILE__ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
